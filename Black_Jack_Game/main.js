b220bc07fe05b83e57e6d7207b84a7dfc5d605f8
DominionCodes <kumator121tech@gmail.com>
2026-05-15 05:34:43 +0100

    sort out the HTML, display issues, Sum and message error

--- Black_Jack_Game/main.js
+++ Black_Jack_Game/main.js
@@ -1,6 +1,6 @@
-let messageGM = document.getElementsByClassName('message')
-let cardGM = document.getElementsByClassName('cards')
-let SumGM = document.getElementsByClassName('Sum')
+let messageGM = document.querySelector('.message')
+let cardGM = document.querySelector('.cards')
+let SumGM = document.querySelector('.Sum')
 
 let TotalSum = 0
 let firstCard = 0
@@ -12,7 +12,7 @@ let hasBlackjack = false
 
 //Generating a Random number that auto change it's self.
 function RandomNumber() {
-    let num = Math.floor(Math.random() * 20)
+    let num = Math.floor(Math.random() * 20) + 1
     // the if statement prevent the number from exceed the 10 or 11
     if (num > 11) {
         return 10
@@ -25,46 +25,39 @@ function RandomNumber() {
 
 
 function startGM() {
+    isAlive = true;
+    hasBlackjack = false;
     firstCard = RandomNumber();
     secondCard = RandomNumber();
     //why the cards in arrays, to display in cardGM
     cards = [firstCard, secondCard]
-    TotalSum += firstCard + secondCard
-    isAlive = true
+    TotalSum = firstCard + secondCard
     GameStart()
-    //     // TotalSum = 0
-
 }
 
 function GameStart() {
+    cardGM.textContent = "Cards : "
     for (let CD = 0; CD < cards.length; CD++) {
-        // cardGM[CD].textContent += cards;
-        cardGM.textContent = cards[CD];
-        console.log(cardGM)
+        cardGM.textContent += cards[CD] + " ";
     }
-    // for (let SG = 0; SG < cards.length; SG++) {
-    //     const element = array[SG];
-            SumGM.textContent = TotalSum
-            console.log(SumGM)
-    // }
+
+    SumGM.textContent = "Sum : " + TotalSum;
     logic()
-    // console.log(TotalSum)
 }
 
-function logic(){
+function logic() {
+    let message = ""
     if (TotalSum > 21) {
-        messsage = 'you out of the Game Thankyou!'
+        message = 'you out of the Game Thankyou!'
         isAlive = false
     } else if (TotalSum === 21) {
-        messsage = 'you Got Black Jack hip! hip!!'
+        message = 'you Got Black Jack hip! hip!!'
         hasBlackjack = true
     } else {
         message = 'Do you want to drew another card'
     }
 
-    messageGM.textContent += message
-    console.log(messageGM)
-
+    messageGM.textContent = message
 }
 function NewCard() {
     if (isAlive && hasBlackjack === false) {
@@ -73,6 +66,6 @@ function NewCard() {
         TotalSum += thirdCard
         GameStart()
     } else {
-        
+
     }
 }
